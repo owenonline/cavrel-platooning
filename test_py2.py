a979714efdbf490968ed7ced2a8918e2b19aa43f
Owen Burns <owenburns88@gmail.com>
2024-03-28 12:34:27 -0400

    switched p2 and p3 roles

--- test_py2.py
+++ test_py2.py
@@ -10,18 +10,18 @@ hostname = socket.gethostname()
 interfaces = socket.getaddrinfo(hostname, None, socket.AF_INET)
 allips = [ip[-1][0] for ip in interfaces]
 
-# msg = "TEST".encode()
-# for _ in range(1000):
-#     for ip in allips:
-#         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
-#         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-#         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-#         sock.bind((ip,0))
-#         sock.sendto(msg, ("255.255.255.255", 37020))
-#         sock.close()
-
+msg = "TEST".encode()
 for _ in range(1000):
-    data, _ = client_sock.recvfrom(1024)
-    data_json = json.loads(data.decode())
-    print(data_json)
-    sleep(0.01)
+    for ip in allips:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        sock.bind((ip,0))
+        sock.sendto(msg, ("255.255.255.255", 37020))
+        sock.close()
+
+# for _ in range(1000):
+#     data, _ = client_sock.recvfrom(1024)
+#     data_json = json.loads(data.decode())
+#     print(data_json)
+#     sleep(0.01)
