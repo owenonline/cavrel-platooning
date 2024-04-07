0c294c94ea46315b013abc7b1852e97ffb6f1287
Owen Burns <burkestree@metrocast.net>
2024-04-07 13:11:49 -0400

    removed code not relevant to testing communications

--- follower_mission_comtest.py
+++ follower_mission_comtest.py
@@ -64,38 +64,6 @@ class UDPPublisher(Node):
 		self.telem_subscription = self.create_subscription(Odometry, '/mavros/global_position/local', self.telem_listener_callback, QoSPresetProfiles.SENSOR_DATA.value, callback_group=MutuallyExclusiveCallbackGroup())
 		self.satellite_subscriber = self.create_subscription(NavSatFix, '/mavros/global_position/global', self.satellite_listener_callback, QoSPresetProfiles.SENSOR_DATA.value, callback_group=MutuallyExclusiveCallbackGroup())
 		self.velocity_subscriber = self.create_subscription(TwistStamped, '/mavros/global_position/gp_vel', self.velocity_listener_callback, QoSPresetProfiles.SENSOR_DATA.value, callback_group=MutuallyExclusiveCallbackGroup())
-		
-        # # setup related to motion
-		# print('setting up motion clients')
-		# self.set_mode_client = self.create_client(SetMode, '/mavros/set_mode')
-		# while not self.set_mode_client.wait_for_service(timeout_sec=1.0):
-		# 	print('set mode service not available, waiting again...')
-			
-		# self.arming_client = self.create_client(CommandBool, '/mavros/cmd/arming')
-		# while not self.arming_client.wait_for_service(timeout_sec=1.0):
-		# 	print('arming service not available, waiting again...')
-
-		# self.killswitch_client = self.create_client(CommandLong, '/mavros/cmd/command')
-		# while not self.killswitch_client.wait_for_service(timeout_sec=1.0):
-		# 	print('killswitch service not available, waiting again...')
-
-		# self.publisher = self.create_publisher(Twist, '/mavros/setpoint_velocity/cmd_vel_unstamped', 20)
-		# self.mission_timer = self.create_timer(0.1, self.mission_timer_callback)
-
-		# # setup kill switch
-		# self.stop_thread = threading.Thread(target=self.listen_for_stop)
-		# self.stop_thread.daemon = True
-		# self.stop_thread.start()
-
-		# # setup mission state
-		# self.mission_status = MISSIONSTART
-
-	def listen_for_stop(self):
-		"""Listens for a KILL command from the user to abort the mission immediately."""
-		while True:
-			command = input()
-			if command.upper() == 'KILL':
-				self.mission_status = ABORT
 
 	def broadcast_timer_callback(self):
 		"""Broadcasts the car's current GPS position to all other cars in the network"""
@@ -105,7 +73,7 @@ class UDPPublisher(Node):
 			return
 
 		# velocity_mag = np.sqrt(self.velocity.twist.linear.x**2 + self.velocity.twist.linear.y**2)
-		msg = json.dumps({"car": self.car, "lat": self.satellite.latitude, "lon": self.satellite.longitude, "time": time(), "abort": self.mission_status == ABORT})
+		msg = json.dumps({"car": self.car, "lat": self.satellite.latitude, "lon": self.satellite.longitude, "time": time()})
 		msg = msg.encode()
 
 		self.broadcast_sock.sendto(msg, ("255.255.255.255", 37020))
@@ -115,10 +83,6 @@ class UDPPublisher(Node):
 
 		data, _ = self.listen_sock.recvfrom(1024)
 		data_json = json.loads(data.decode())
-
-		# if one of the cars failed, stop the mission immediately
-		if data_json['abort']:
-			self.mission_status = ABORT
 
 		if data_json['car'] <= self.car:
 			current_lat = self.satellite.latitude
@@ -154,7 +118,6 @@ class UDPPublisher(Node):
 				x2, y2, time2 = self.car_positions[0][1]
 				print(f"lead vehicle most recent update:\n\tx:\ty:\n{time1}\t{x1}\t{y1}\n{time2}\t{x2}\t{y2}\n")
                 
-
 	def telem_listener_callback(self, msg):
 		"""Saves the latest telemetry message"""
 		self.telem = msg
@@ -167,178 +130,6 @@ class UDPPublisher(Node):
 		"""Saves the latest velocity message"""
 		self.velocity = msg
 
-	def get_goal_motion(self):
-		targets = []
-		for i in range(self.car - 1, -1, -1):
-			(x1, y1, time1), (x2, y2, time2) = self.car_positions[i]
-
-			velocity = np.sqrt((x2 - x1)**2 + (y2 - y1)**2)/(time2 - time1)
-			heading = np.arctan2(y2 - y1, x2 - x1)
-			targets.append((x2, y2, heading, velocity, self.car - 1 - i))
-
-		def minimization_objective(params):
-			v, head = params
-			x, y = self.telem.pose.pose.position.x, self.telem.pose.pose.position.y
-
-			total_cost = 0
-			for target in targets:
-				x_target, y_target, head_target, v_target, position = target
-				goal_follow_distance = FOLLOW_DISTANCE*position + CAR_LENGTH*(position - 1)
-				total_cost += goal_follow_distance - np.sqrt((x_target + v_target*np.cos(head_target) - x - v*np.cos(head))**2 + (y_target + v_target*np.sin(head_target) - y - v*np.sin(head))**2)
-
-			return total_cost
-		
-		res = minimize(minimization_objective, [1.0, 0.0])
-
-		return res, targets
-	
-	def pd_controller(self, v, v_ego):
-		accel = KP*(v - v_ego) + KD*(v - v_ego)/BROADCAST_INTERVAL
-		return accel
-	
-	def stanley_controller(self, x_ego, y_ego, head_ego, v_ego, head, target_x, target_y):
-		e = abs((x_ego - target_x) * np.sin(head) - (y_ego - target_y) * np.cos(head))
-		steer = np.arctan2(K*e/v_ego)+(head_ego - head)
-
-	def mission_timer_callback(self):
-		"""Main loop for vehicle control. Handles the arming, moving, and disarming of the rover."""
-
-		# TODO: Make sure this waits for the cars in front of the current car rather than all of the cars, or some other heuristic
-		# wait until we've received position data from all the other cars before starting the mission
-		# if len(list(self.car_positions.values())) < NUM_CARS - 1:
-		# 	return
-
-		# start the chain of events that arms the rover
-		if self.mission_status == MISSIONSTART:
-			print("switching to offboard mode")
-			set_mode_req = SetMode.Request()
-			set_mode_req.base_mode = 0
-			set_mode_req.custom_mode = "OFFBOARD"
-			set_mode_future = self.set_mode_client.call_async(set_mode_req)
-			set_mode_future.add_done_callback(self.init_callback)
-			self.mission_status = ARMING
-			return
-		
-		if self.mission_status == ARMING:
-			# px4 requires a stream of setpoint messages to be sent to the rover in order to arm
-			msg = Twist()
-			msg.linear.x = 0.0
-			msg.linear.y = 0.0
-			msg.linear.z = 0.0
-
-			msg.angular.x = 0.0
-			msg.angular.y = 0.0
-			msg.angular.z = 0.0
-			
-			self.publisher.publish(msg)
-			return
-		
-		if self.mission_status == MOVING:
-			msg = Twist()
-
-			if time() - self.start_time > 5:
-				print("...stopped, disarming")
-				msg.linear.x = 0.0
-				msg.linear.y = 0.0
-				msg.linear.z = 0.0
-
-				msg.angular.x = 0.0
-				msg.angular.y = 0.0
-				msg.angular.z = 0.0
-				
-				self.publisher.publish(msg)
-
-				disarm_req = CommandBool.Request()
-				disarm_req.value = False
-				disarm_future = self.arming_client.call_async(disarm_req)
-				disarm_future.add_done_callback(self.disarm_callback)
-
-				self.mission_status = DISARMING
-			else:
-				# get the position, heading, and velocity for each preceding vehicle, then minimize to get the target speed and heading
-				res, targets = self.get_goal_motion()				
-
-				if not res.success:
-					self.mission_status = ABORT
-					return
-				
-				v, head = res.x
-
-				# get the motion of the ego vehicle
-				v_ego = np.sqrt(self.velocity.twist.linear.x**2 + self.velocity.twist.linear.y**2)
-				_, _, head_ego = tf_transformations.euler_from_quaternion([self.telem.pose.pose.orientation.x, self.telem.pose.pose.orientation.y, self.telem.pose.pose.orientation.z, self.telem.pose.pose.orientation.w])
-				x_ego, y_ego = self.telem.pose.pose.position.x, self.telem.pose.pose.position.y
-
-				# update velocity and yaw
-				accel = self.pd_controller(v, v_ego)
-				steer = self.stanley_controller(x_ego, y_ego, head_ego, v_ego, head, targets[0][0], targets[0][1])
-				steer = np.clip(steer, -MAX_STEER, MAX_STEER)
-				head_ego += v_ego/WHEELBASE*np.tan(steer)*BROADCAST_INTERVAL
-				v_ego += accel*BROADCAST_INTERVAL
-
-				# send updated control message
-				msg.linear.x = v_ego*np.cos(head_ego)
-				msg.linear.y = v_ego*np.sin(head_ego)
-				msg.linear.z = 0.0
-
-				msg.angular.x = 0.0
-				msg.angular.y = 0.0
-				msg.angular.z = 0.0
-
-				self.publisher.publish(msg)
-			return
-		
-		if self.mission_status == DISARMING:
-			msg = Twist()
-			msg.linear.x = 0.0
-			msg.linear.y = 0.0
-			msg.linear.z = 0.0
-
-			msg.angular.x = 0.0
-			msg.angular.y = 0.0
-			msg.angular.z = 0.0
-			
-			self.publisher.publish(msg) # continue publishing stop messages until disarmed
-			return
-	
-		if self.mission_status == MISSIONCOMPLETE:
-			return
-		
-		# if the mission is aborted, turn off all motors immediately.
-		if self.mission_status == ABORT:
-			print("ABORTING")
-			emergency_disarm_req = CommandLong.Request()
-			emergency_disarm_req.broadcast = False
-			emergency_disarm_req.command = 400
-			emergency_disarm_req.confirmation = 0
-			emergency_disarm_req.param1 = 0.0
-			emergency_disarm_req.param2 = 21196.0
-			emergency_disarm_req.param3 = 0.0
-			emergency_disarm_req.param4 = 0.0
-			emergency_disarm_req.param5 = 0.0
-			emergency_disarm_req.param6 = 0.0
-			emergency_disarm_req.param7 = 0.0
-
-			self.killswitch_client.call_async(emergency_disarm_req)
-			self.mission_status = MISSIONCOMPLETE
-			return
-		
-	def init_callback(self, future):
-		print("...in offboard mode, arming")
-		sleep(4) # wait for the stream of messages to be long enough to allow arming
-		arm_req = CommandBool.Request()
-		arm_req.value = True
-		arm_future = self.arming_client.call_async(arm_req)
-		arm_future.add_done_callback(self.arm_callback)
-
-	def arm_callback(self, future):
-		print("...armed, moving")
-		self.start_time = time()
-		self.mission_status = MOVING
-
-	def disarm_callback(self, future):
-		print("...disarmed, mission complete")
-		self.mission_status = MISSIONCOMPLETE
 
 rclpy.init(args=None)
 udp_publisher = UDPPublisher(int(input()))
