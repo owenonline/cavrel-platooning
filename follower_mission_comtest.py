2c8e05914c6f94c7006d7daf19b48777e2485301
Owen Burns <burkestree@metrocast.net>
2024-04-07 13:14:22 -0400

    changed udp code new port is used instead of reusing port

--- follower_mission_comtest.py
+++ follower_mission_comtest.py
@@ -46,11 +46,9 @@ class UDPPublisher(Node):
         # setup related to udp communication
 		self.broadcast_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
 		self.broadcast_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-		self.broadcast_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		self.broadcast_sock.bind(("", 37020))
+		self.broadcast_sock.bind(("", int(f"3702{car}")))
 		self.listen_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		self.listen_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		self.listen_sock.bind(("", 37020))
+		self.listen_sock.bind(("", int(f"3702{car}")))
 
 		interfaces = socket.getaddrinfo(host=socket.gethostname(), port=None, family=socket.AF_INET)
 		self.allips = [ip[-1][0] for ip in interfaces]
