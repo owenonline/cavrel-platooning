7f9afee5287c324687331ae319bb43f2b518ae59
Owen Burns <burkestree@metrocast.net>
2024-05-01 17:52:54 -0400

    added missing coords to local param self

--- missions/follower_mission.py
+++ missions/follower_mission.py
@@ -42,7 +42,7 @@ WHEELBASE = 0.48
 CAR_LENGTH = 0.779
 FOLLOW_DISTANCE = 2.0 # meters behind the immediate preceding vehicle, 4 meters behind the second preceding vehicle, etc.
 DUE_EAST = 90
-SPEED_LIMIT = 0.3
+SPEED_LIMIT = 0.05
 geodesic = pyproj.Geod(ellps='WGS84')
 center_latitude = (28.607980 + 28.607292) / 2
 center_longitude = (-81.195662 + -81.194750) / 2
